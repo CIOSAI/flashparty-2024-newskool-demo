2e01044400a8d329680756b7c8ea2149f72698d5
CIOSAI <jyinteractive.tw@gmail.com>
2024-09-23 21:34:53 +0800

    finally something nice to watch

--- src/index.js
+++ src/index.js
@@ -6,26 +6,31 @@ let canvas = document.querySelector('#myCanvas');
 canvas.addEventListener('fullscreenchange', ()=>{
   with (paper) {
     let background = new Path.Rectangle(new Point(0,0),view.size);
-    background.fillColor = Globals.palette[3];
+    background.fillColor = Globals.palette[0];
 
     let foregroundLayer = new Layer();
     let typo = new PointText({
-      fillColor: Globals.palette[0],
+      fillColor: Globals.palette[1],
       fontFamily: 'Kaukhia',
+      fontSize: 144,
       content: 'This is the Kaukhia font'
     });
-    let path = new Path();
-		path.strokeColor = Globals.palette[1];
-		let start = new Point(100, 100);
+    let path = new Path({
+      strokeColor: Globals.palette[2],
+      strokeWidth: 30,
+    });
+		let start = new Point(view.size.divide(2));
 		path.moveTo(start);
-		path.lineTo(start.add([ 200, -50 ]));
+		path.lineTo(start.add([ view.size.height / 8, 0 ]));
 		
     view.draw();
 		view.onFrame = function(event) {
       let TIME = event.time;
       let FRAMECOUNT = event.count;
       path.rotate(event.delta*32);
-      typo.position = [600+Math.cos(TIME)*200, 600+Math.sin(TIME)*200];
+      typo.position = view.size.divide(2).add(
+        (new Size(Math.cos(TIME), Math.sin(TIME))).multiply(200)
+      );
 		}
 	}
 });
