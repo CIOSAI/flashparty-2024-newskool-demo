a91aace1ad5087fa7baed5370cf2a550f4acd755
CIOSAI <jyinteractive.tw@gmail.com>
2024-09-23 22:54:39 +0800

    animated via tween

--- src/index.js
+++ src/index.js
@@ -5,32 +5,46 @@ window.onload = function() {
 let canvas = document.querySelector('#myCanvas');
 canvas.addEventListener('fullscreenchange', ()=>{
   with (paper) {
+    if (document.fullscreenElement == null) { return; }
+
+    // kept for reminder
+
+    // let typo = new PointText({
+    //   fillColor: Globals.palette[1],
+    //   fontFamily: 'Kaukhia',
+    //   fontSize: 144,
+    //   content: 'This is the Kaukhia font'
+    // });
+    
     let background = new Path.Rectangle(new Point(0,0),view.size);
     background.fillColor = Globals.palette[0];
 
     let foregroundLayer = new Layer();
-    let typo = new PointText({
-      fillColor: Globals.palette[1],
-      fontFamily: 'Kaukhia',
-      fontSize: 144,
-      content: 'This is the Kaukhia font'
-    });
-    let path = new Path({
-      strokeColor: Globals.palette[2],
-      strokeWidth: 30,
-    });
-		let start = new Point(view.size.divide(2));
-		path.moveTo(start);
-		path.lineTo(start.add([ view.size.height / 8, 0 ]));
-		
+
+    let loadingCircs = [];
+    for(let i=0; i<8; i++){
+      let circ = new Path.Circle({
+        center: (new Point(1+i,1)).multiply(view.size.height/16),
+        radius: view.size.height/32,
+        scaling: 0.00001,
+        fillColor: Globals.palette[3],
+        applyMatrix: false
+      });
+      loadingCircs.push(circ);
+    }
+    for(let i=0; i<8; i++){
+      loadingCircs[i].tween(500*i+1).then(_=>{
+        loadingCircs[i].tween({
+          scaling: 0.0001
+        }, {
+          scaling: 1
+        }, {
+          duration: 500, 
+          easing: 'easeOutQuint'
+        });
+      });
+    }
+
     view.draw();
-		view.onFrame = function(event) {
-      let TIME = event.time;
-      let FRAMECOUNT = event.count;
-      path.rotate(event.delta*32);
-      typo.position = view.size.divide(2).add(
-        (new Size(Math.cos(TIME), Math.sin(TIME))).multiply(200)
-      );
-		}
 	}
 });
