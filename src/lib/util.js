379c05592a3a151d635bb78b0abed7ea4cbfee7e
CIOSAI <jyinteractive.tw@gmail.com>
2024-09-23 21:28:39 +0800

    removed color utils cuz it did not help at all

---
+++ src/lib/util.js
@@ -0,0 +1,15 @@
+const Globals = {
+  palette: [
+    new paper.Color('#fff923'),
+    new paper.Color('#ff8d12'),
+    new paper.Color('#568b24'),
+    new paper.Color('#282828'),
+  ]
+};
+
+// let palette = [
+//   new Color('#fff923'),
+//   new Color('#ff8d12'),
+//   new Color('#568b24'),
+//   new Color('#282828'),
+// ];
