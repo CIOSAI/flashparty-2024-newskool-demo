2f3fd0a67f90fefb78818bfe137ebf9ccdb9fb4d
CIOSAI <jyinteractive.tw@gmail.com>
2024-09-25 19:23:47 +0800

    cube and scroller

--- src/index.js
+++ src/index.js
@@ -21,8 +21,9 @@ canvas.addEventListener('fullscreenchange', ()=>{
 
     let foregroundLayer = new Layer();
 
+    let loadingCircAmt = 10;
     let loadingCircs = [];
-    for(let i=0; i<10; i++){
+    for(let i=0; i<loadingCircAmt; i++){
       let circ = new Path.Circle({
         center: (new Point(1+i,0)).multiply(view.size.height/16),
         radius: view.size.height/32,
@@ -33,8 +34,8 @@ canvas.addEventListener('fullscreenchange', ()=>{
       loadingCircs.push(circ);
     }
     for(let j=0; j<4; j++){
-      delay(Globals.BEAT_DUR*10*j, _=>{
-        for(let i=0; i<10; i++){
+      delay(Globals.BEAT_DUR*loadingCircAmt*j, _=>{
+        for(let i=0; i<loadingCircAmt; i++){
           loadingCircs[i].position.y += view.size.height/16;
           loadingCircs[i].visible = false;
           delay(Globals.BEAT_DUR*i, _=>{
@@ -59,6 +60,70 @@ canvas.addEventListener('fullscreenchange', ()=>{
         }
       });
     }
+    delay(Globals.BEAT_DUR*loadingCircAmt*4, _=>{
+      for(let i=0; i<loadingCircAmt; i++){
+        loadingCircs[i].visible = false;
+      }
+    });
+
+    let layer2 = new Layer();
+    let pseudoCube = new Path.RegularPolygon(view.center, 6, view.size.height/8);
+    pseudoCube.strokeColor = Globals.palette[3];
+    pseudoCube.strokeWidth = view.size.height/96;
+    pseudoCube.visible = false;
+    delay(Globals.BEAT_DUR*loadingCircAmt*4, _=>{
+      pseudoCube.visible = true;
+      pseudoCube.tween(Globals.BEAT_DUR*loadingCircAmt*8).onUpdate = event => {
+        let ease = n => n>0.5?1:Math.sin(n*TAU-PI/2)*.5+.5;
+        let t = event.factor;
+        let i_t = ~~(t*16), f_t = (t*16)%1;
+        t = i_t + ease(f_t);
+
+        let verts = [[1,-1,-1],[-1,-1,-1],[-1,-1,1],[-1,1,1],[1,1,1],[1,1,-1]];
+        for(let vert of verts){
+          let p = new Point(vert[0], vert[2]);
+          p = p.rotate(45+Math.cos(t*2.25)*20);
+          vert[0] = p.x; vert[2] = p.y;
+          p = new Point(vert[1], vert[2]);
+          p = p.rotate(-45+Math.sin(t*1.75)*20);
+          vert[1] = p.x; vert[2] = p.y;
+        }
+
+        for(let i=0; i<6; i++){
+          pseudoCube.segments[i].point = view.center.add( proj(verts[i],4).multiply(view.size.height/2) );
+        }
+      };
+    });
+
+    foregroundLayer.activate();
+    let typo = new PointText({
+      fillColor: Globals.palette[1],
+      fontFamily: 'Kaukhia',
+      fontSize: 72,
+      justification: "center",
+      point: new Point(view.center.x,0),
+      // point: view.center,
+      applyMatrix: false
+    });
+    let scroller = [
+      "¡Hola todos!", "Soy CIOSAI de Taiwán", "no puedo viajar a allá", "¡pero yo puedo hacer demo!",
+      "normalmente escribo glsl", "esta vez hicelo con paper.js", "la fuente llama Kaukhia", "¡divertirse la party!"
+    ];
+    delay(Globals.BEAT_DUR*loadingCircAmt*4, _=>{
+      for(let i=0; i<8; i++){
+        delay(Globals.BEAT_DUR*20*i, _=>{
+          typo.content = scroller[i];
+          typo.tween({
+            "position.y": 0-36
+          }, {
+            "position.y": view.size.height+36
+          }, {
+            easing: "easeInOutCubic",
+            duration: Globals.BEAT_DUR*20
+          });
+        });
+      }
+    });
 
     view.draw();
 	}
--- src/lib/util.js
+++ src/lib/util.js
@@ -9,6 +9,17 @@ const Globals = {
   ]
 };
 
+const PI = Math.PI;
+const TAU = PI*2;
+
 function delay(seconds, func) {
   (new paper.Path()).tween(seconds+0.000001).then(func);
+}
+
+function scale3(vec3, sca) {
+  return [vec3[0]*sca, vec3[1]*sca, vec3[2]*sca];
+}
+
+function proj(vec3, off=0) {
+  return new paper.Point(vec3[0]/(vec3[2]+off), vec3[1]/(vec3[2]+off));
 }
